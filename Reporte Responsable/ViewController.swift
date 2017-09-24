d29e8189a73ce53c85f3040ef1ceaf3f4d15803e
Rodrigo Moreno <moreno.rdr@gmail.com>
2017-09-24 15:36:25 -0500

    + Avances en el formulario

--- Reporte Responsable/ViewController.swift
+++ Reporte Responsable/ViewController.swift
@@ -10,21 +10,55 @@ import Alamofire
 import MapKit
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate, MKMapViewDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate, MKMapViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var contact: UITextField!
     @IBOutlet weak var phone: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var street: UITextField!
+    @IBOutlet weak var number: UITextField!
+    @IBOutlet weak var neighborhood: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var zipCode: UITextField!
+    @IBOutlet weak var typeProperty: UITextField!
+    @IBOutlet weak var damages: UITextField!
+    @IBOutlet weak var damagesLocation: UITextField!
+    @IBOutlet weak var levels: UITextField!
+    @IBOutlet weak var habitants: UITextField!
+    @IBOutlet weak var isEvacuated: UITextField!
     @IBOutlet weak var observations: UITextField!
     @IBOutlet weak var photo: UIImageView!
-    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var elementPhoto: UIImageView!
     @IBOutlet weak var takePhotoButton: UIButton!
+    @IBOutlet weak var takeElementsPhotoButton: UIButton!
     @IBOutlet weak var sendReportButton: UIButton!
     
+    weak var activeField: UITextField?
+    
+    let typePropertyData = ["Hospital", "Escuela", "Estancia Infantil", "Departamento", "Casa", "Comercio",
+        "Oficinas Públicas", "Oficinas Privadas", "Industria", "Centro de Reunión", "Estacionamiento",
+        "Recreativo", "Otro"]
+    let damagesData = ["Derrumbe Total", "Derrumbe Parcial", "Grietas en Muros y Acabados", "Grietas en Trabes y Columnas", "Asentamiento o Hundimiento", "Daños en vidrios y cancelerías"]
+    let damagesLocationData = ["Interior", "Exterior", "Ambas"]
+    let isEvacuatedData = ["Si", "No", "Parcialmente"]
+    
+    
     let locationManager = CLLocationManager()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setUpKeyboardNotifications()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.scrollView.keyboardDismissMode = .onDrag
+        
+        
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         if (CLLocationManager.authorizationStatus() == .notDetermined) {
@@ -36,18 +70,158 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         
         let addAnnotationTap = UILongPressGestureRecognizer(target: self, action: #selector(addAnnotation(_:)))
         self.map.addGestureRecognizer(addAnnotationTap)
+        
+        // Toolbar para UIPickerView
+        let toolbarPickerView = UIToolbar()
+        let toolbarPickerViewFlexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let toolbarPickerViewOkButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(pickerViewDone(_:)))
+        toolbarPickerView.backgroundColor = .gray
+        toolbarPickerView.sizeToFit()
+        toolbarPickerView.setItems([toolbarPickerViewFlexSpace, toolbarPickerViewOkButton], animated: false)
+        toolbarPickerView.isUserInteractionEnabled = true
+        
+        // Tipo de propiedad
+        let typePropertyPickerView = UIPickerView()
+        typePropertyPickerView.delegate = self
+        typePropertyPickerView.tag = 1
+        self.typeProperty.inputView = typePropertyPickerView
+        self.typeProperty.inputAccessoryView = toolbarPickerView
+        
+        // Principal afectación
+        let damagesPickerView = UIPickerView()
+        damagesPickerView.delegate = self
+        damagesPickerView.tag = 2
+        self.damages.inputView = damagesPickerView
+        self.damages.inputAccessoryView = toolbarPickerView
+        
+        // Ubicación del daño
+        let damagesLocationPickerView = UIPickerView()
+        damagesLocationPickerView.delegate = self
+        damagesLocationPickerView.tag = 3
+        self.damagesLocation.inputView = damagesLocationPickerView
+        self.damagesLocation.inputAccessoryView = toolbarPickerView
+        
+        // Ya fue evacuado
+        let isEvacuatedPickerView = UIPickerView()
+        isEvacuatedPickerView.delegate = self
+        isEvacuatedPickerView.tag = 4
+        self.isEvacuated.inputView = isEvacuatedPickerView
+        self.isEvacuated.inputAccessoryView = toolbarPickerView
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.removeKeyboardNotifications()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+        for touch in touches {
+            let touchedView = touch.view
+            if let _view = touchedView {
+                if _view == view {
+                    self.view.endEditing(true)
+                }
+            }
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch pickerView.tag {
+        case 1:
+            return self.typePropertyData.count
+        case 2:
+            return self.damagesData.count
+        case 3:
+            return self.damagesLocationData.count
+        case 4:
+            return self.isEvacuatedData.count
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch pickerView.tag {
+        case 1:
+            return self.typePropertyData[row]
+        case 2:
+            return self.damagesData[row]
+        case 3:
+            return self.damagesLocationData[row]
+        case 4:
+            return self.isEvacuatedData[row]
+        default:
+            return nil
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch pickerView.tag {
+        case 1:
+            self.typeProperty.text = self.typePropertyData[row]
+        case 2:
+            self.damages.text = self.damagesData[row]
+        case 3:
+            self.damagesLocation.text = self.damagesLocationData[row]
+        case 4:
+            self.isEvacuated.text = self.isEvacuatedData[row]
+        default:
+            break
+        }
+    }
+    
+    @objc func pickerViewDone (_ sender: UIBarButtonItem) {
+        print("DONE")
+        self.typeProperty.resignFirstResponder()
+        self.damages.resignFirstResponder()
+        self.damagesLocation.resignFirstResponder()
+        self.isEvacuated.resignFirstResponder()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.activeField = textField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.activeField = nil
+    }
+    
+    @objc func removeKeyboardNotifications(){
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func setUpKeyboardNotifications(){
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+
+    
+    @objc func keyboardDidShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+            self.scrollView.contentInset = contentInsets
+            self.scrollView.scrollIndicatorInsets = contentInsets
+            var aRect = self.view.frame
+            aRect.size.height -= keyboardSize.size.height
+            if (!aRect.contains((self.activeField?.frame.origin)!)) {
+                self.scrollView.scrollRectToVisible((self.activeField?.frame)!, animated: true)
+            }
+        }
+    }
+    
+    @objc func keyboardWillBeHidden(notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // TODO
         let photoTaken = info[UIImagePickerControllerOriginalImage] as! UIImage
         self.photo.image = photoTaken
         
@@ -76,6 +250,9 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         picker.sourceType = .camera
         self.present(picker, animated: true, completion: nil)
     }
+
+    @IBAction func captureElementPhoto(_ sender: UIButton) {
+    }
     
     @objc func addAnnotation(_ sender: UIGestureRecognizer) -> Void {
         let point = self.map.convert(sender.location(in: self.map), toCoordinateFrom: self.map)
@@ -93,7 +270,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         } else {
             self.takePhotoButton.isEnabled = false
             self.sendReportButton.isEnabled = false
-            self.upload(image: self.photo.image!, progressCompletion: {percent in
+            self.upload(image: [self.photo.image!, self.elementPhoto.image!], progressCompletion: {percent in
                 
                 }, completion: {response in
                     self.cleanAllData()
@@ -118,8 +295,9 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
 }
 
 extension ViewController {
-    func upload(image: UIImage, progressCompletion: @escaping (_ percent: Float) -> Void, completion: @escaping(_ response: [String]) -> Void) {
-        guard let imageData = UIImageJPEGRepresentation(image, 0.9) else {
+    func upload(image: [UIImage], progressCompletion: @escaping (_ percent: Float) -> Void, completion: @escaping(_ response: [String]) -> Void) {
+        guard let imageData = UIImageJPEGRepresentation(image[0], 0.7),
+        let imageElementsData = UIImageJPEGRepresentation(image[1], 0.7) else {
             print("Error: Could not get JPEG representation for UIImage")
             return
         }
@@ -131,6 +309,7 @@ extension ViewController {
             multipartFormData.append(String(self.map.annotations[0].coordinate.latitude).data(using: String.Encoding.utf8)!, withName: "latitud")
             multipartFormData.append(String(self.map.annotations[0].coordinate.longitude).data(using: String.Encoding.utf8)!, withName: "longitud")
             multipartFormData.append(imageData, withName: "file", fileName: "file.jpg", mimeType: "image/jpeg")
+            multipartFormData.append(imageElementsData, withName: "file_elementos", fileName: "file_elementos.jpg", mimeType: "image/jpeg")
         }, with: IngRouter.report, encodingCompletion: {encodingResult in
             switch encodingResult {
             case .success(let upload, _, _):
